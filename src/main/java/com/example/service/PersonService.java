75a32aca88e6276f792d8154d16b73e589c8247c
Moetaz-Alfy <Workalfy4@gmail.com>
2026-07-15 20:48:56 +0300

    testing webhook

--- src/main/java/com/example/service/PersonService.java
+++ src/main/java/com/example/service/PersonService.java
@@ -39,6 +39,13 @@ public class PersonService {
 		p.setFirstName("AbdulHalim");
 		p.setLastName("Mahmoud");
 		persons.add(p);
+
+		p = new Person();
+		p.setId("5");
+		p.setAge(23);
+		p.setFirstName("Moataz");
+		p.setLastName("Gamal");
+		persons.add(p);
 		
 	}
 	public Person getPerson(String id) {
